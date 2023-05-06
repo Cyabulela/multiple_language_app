96bb53b21ea6fbf4914332f2cbae47f2ad67e590
Cyabulela <95098257+Cyabulela@users.noreply.github.com>
2023-05-06 04:41:41 +0200

    commit app but menu is still not working properly

---
+++ app/src/main/java/com/example/multiplplelanguageapplication/domain/LanguageSetUp.java
@@ -0,0 +1,39 @@
+package com.example.multiplplelanguageapplication.presentation;
+
+import static com.example.multiplplelanguageapplication.data.Language.Afrikaans;
+import static com.example.multiplplelanguageapplication.data.Language.English;
+import static com.example.multiplplelanguageapplication.data.Language.IsiZulu;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.example.multiplplelanguageapplication.R;
+import com.example.multiplplelanguageapplication.data.Language;
+import com.example.multiplplelanguageapplication.data.preference.PreferenceManagement;
+import com.example.multiplplelanguageapplication.data.repository.LanguageConfigImpl;
+import com.example.multiplplelanguageapplication.domain.repository.LanguageConfig;
+
+public class LanguageSetUp {
+
+    private Language language;
+    private static LanguageConfig languageConfig;
+
+    public LanguageSetUp(Context context) {
+        languageConfig = new LanguageConfigImpl(new PreferenceManagement(context));
+    }
+
+    public static void setLanguage(@NonNull int itemId) {
+        if (itemId == R.id.language_english) {
+            languageConfig.changeLanguage(English);
+        } else if (itemId == R.id.language_zulu) {
+            languageConfig.changeLanguage(IsiZulu);
+        } else {
+            languageConfig.changeLanguage(Afrikaans);
+        }
+    }
+
+    public Language getLanguage() {
+        return languageConfig.getSelectedLanguage();
+    }
+}
--- app/src/main/java/com/example/multiplplelanguageapplication/presentation/SetUp.java
+++
@@ -1,2 +0,0 @@
-package com.example.multiplplelanguageapplication.presentation;public class SetUp {
-}
